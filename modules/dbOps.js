1f046fa9f5bc3eac7788382aa201edafce4863eb
TimmyB <timmybofficialyt@gmail.com>
2019-06-06 12:19:18 -0400

    setting up DB operations

--- modules/dbOps.js
+++ modules/dbOps.js
@@ -1,7 +1,9 @@
-const Database = require('better-sqlite3');
-// connect to libraries database ( create if doesnt exist
-const db = new Database("libraries.db");
 
+const Database = require('better-sqlite3');
+var fs = require('fs');
+// connect to libraries database ( create if doesnt exist
+const racksFolder = "./racks/"
+const db = new Database(racksFolder+"libraries.db");
 // check to see if database initialized
 var qry = db.prepare(`SELECT name
     FROM sqlite_master
@@ -13,7 +15,7 @@ if (row === undefined) {
     console.log("WARNING: database appears empty; initializing it.");
     const sqlInit = `
         CREATE TABLE libraries (
-            id   INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY,
             name TEXT,
             path TEXT,
             created TEXT
@@ -23,21 +25,81 @@ if (row === undefined) {
 }
 console.log("database exists now, if it didn't already.");
 
-getLibraries();
+getRacks();
 
-
+createRack('test 123')
 //Getters
 
-// get libraries
-function getLibraries() {
+// get Racks
+
+function getRacks() {
     var qry = db.prepare(`SELECT * FROM libraries;`);
     var data = qry.get();
     if (data === undefined) {
-        console.log("No Libraries Found")
+        console.log("No Racks Found")
     }
 }
 
-//Setters
+//Creators
+
+// Create Rack
+function createRack(rackName) {
+    const qry = db.prepare(`INSERT INTO libraries VALUES (NULL, @name, @path, date('now'))`);
+    var rackpath = rackName.replace(/ /g, "_");
+    var fullPath = racksFolder + rackpath;
+    //check to see if library path exists - should also add a check for db obviously.
+    if (!fs.existsSync(fullPath)) { 
+        console.log("[INFO] Creating Rack folder and database");
+        fs.mkdirSync(fullPath);
+        qry.run({
+            name: rackName,
+            path: rackpath
+        });
+         // we dont want to create a library folder if it already exists
+         // perhaps check to see if there is a db here and import it into libraries;
+        createRackDb(fullPath);
+    }else{
+        console.log("[ERROR] Rack folder exists");
+    }
+}
 
 
-module.exports = dbOps;
+
+
+function createRackDb(path){
+    console.log(path)
+    const rackDb = new Database(path+"/rack.db");
+    
+    const sqlInit = `
+        CREATE TABLE items (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            description TEXT,
+            publisher INT,
+            series INT,
+            tags INT,
+            publish_date TEXT,
+            path TEXT,
+            added TEXT,
+            modified TEXT
+        );
+        CREATE TABLE tags (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+        );
+        CREATE TABLE series (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+        );
+        CREATE TABLE publishers (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+        );
+        `;
+    rackDb.exec(sqlInit);
+}
+
+
+
+
+module.exports = {};
